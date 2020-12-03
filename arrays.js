0303b9b48dc2804c9b5d6a7ae4b489fcc095e367
sunam RImal Nepali <tsunamr@gmail.com>
2020-12-03 21:43:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,23 +1,2 @@
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-var array= ['foo', 1]
-function addElementToBeginningOfArray(){
-
-
-return array }
-
-function destructivelyAddElementToBeginningOfArray(){
-
-  return array
-}
-var array = new array['foo', 1];
-function destructivelyAddElementToBeginningOfArray(){
-  return array
-}
-function destructivelyAddElementToBeginningOfArray(){
-  return array
-}
-function addElementToEndOfArray(){
-  array.push('foo')
-  return array
-}
