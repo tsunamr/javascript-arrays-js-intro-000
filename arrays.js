2c5359fabd091cb5131f9badc16448474703d310
sunam RImal Nepali <tsunamr@gmail.com>
2020-12-03 20:53:42 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var array= ['foo', 1]
+function addElementToBeginningOfArray(){
+
+
+return array }
+
+function destructivelyAddElementToBeginningOfArray(){
+
+  return array
+}
+var array = new array('foo');
+function destructivelyAddElementToBeginningOfArray(){
+  return array
+}
+function addElementToEndOfArray(){
+  array.push('1')
+  return array
+}
