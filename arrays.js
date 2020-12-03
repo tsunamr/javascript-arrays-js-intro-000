82afbacb156538b419b6f4260a53322541877174
sunam RImal Nepali <tsunamr@gmail.com>
2020-12-03 21:47:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
-  return array, element
+  return chocolateBars
 }
